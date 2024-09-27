04b12c195c20390de48f80d525d7e355de6494f0
BalogiAlex <balogialex@gmail.com>
2024-09-27 13:12:10 +0200

    hozzáadtam a labor02 fájlt

---
+++ labor02.py
@@ -0,0 +1,18 @@
+#szam = 10
+'''
+szam2 = 20
+print(szam)
+'''
+
+print("Szia", "due!")
+print("Szia due!")
+print("szia", "due", sep="-")
+print("szia", "\ndue\t!")
+print("szia", "due", end='!\n')
+print("""Szép napunk 
+lesz ma!""")
+
+felhasznalo_neve="Józsi"
+print("----------")
+print(felhasznalo_neve)
+print("Szia", felhasznalo_neve)
